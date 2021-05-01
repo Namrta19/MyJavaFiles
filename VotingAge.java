eb331f6b10f21d41abf3445e91e6cecd7f20ec25
Namrta <namrta1819@gmail.com>
2021-05-01 15:31:53 +0100

    I am Tester

---
+++ VotingAge.java
@@ -0,0 +1,13 @@
+public class VotingAge {
+
+    public static void main(String[] args)
+    {
+        int Age = 19;
+
+                if (Age<18) {
+                    System.out.println("Not Eligible to vote");
+                }else if (Age>18) {
+                    System.out.println("Eligible to vote");
+                }
+    }
+}
